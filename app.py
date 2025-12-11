3bb6640b00adfadbffedcd3b4b6a588271a56cdd
Benjamin Rivera <ben.paul.riv@gmail.com>
2025-12-11 11:44:59 -0600

    First functional json file abstraction of webpage text

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+import json
+from flask import Flask, jsonify, render_template
 
 
 # configure application
@@ -8,7 +9,9 @@ app.secret_key = "Final Project!"
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    with open("translations/en-us.json", "r", encoding="utf-8") as file:
+        content_dict = json.loads(file.read())
+    return render_template("index.html", content_title=content_dict["index_main_title"], content_body=content_dict["index_main_body"])
 
 if __name__ == "__main__":
     app.run(debug=True)
