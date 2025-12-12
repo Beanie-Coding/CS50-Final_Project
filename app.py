2d1f36ae158ce9a049c466446afd09dcbfba27ac
Benjamin Rivera <ben.paul.riv@gmail.com>
2025-12-11 19:57:11 -0600

    Functional homepage

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import json
-from flask import Flask, jsonify, render_template
+import sqlite3
+from flask import Flask, render_template, session, redirect, url_for, request
 
 
 # configure application
@@ -9,7 +10,9 @@ app.secret_key = "Final Project!"
 
 @app.route("/")
 def index():
-    with open("translations/en-us.json", "r", encoding="utf-8") as file:
+    english = "translations/en-us.json"
+    spanish = "translations/es.json"
+    with open(spanish, "r", encoding="utf-8") as file:
         content_dict = json.loads(file.read())
     return render_template("index.html", content_title=content_dict["index_main_title"], content_body=content_dict["index_main_body"])
 
